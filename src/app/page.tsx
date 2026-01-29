ac5e96519e88adb50d64b9feff7bfebacee17f8b
Jose Luis <josephsiul15@gmail.com>
2026-01-28 18:45:29 -0600

    favicon change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Upgrade from '@/components/Home/upgrade'
 import Perks from '@/components/Home/perks'
 import { Metadata } from 'next'
 export const metadata: Metadata = {
-  title: 'CrypGo',
+  title: 'Orix',
 }
 
 export default function Home() {
